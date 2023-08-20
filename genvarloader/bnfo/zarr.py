1e29e9245e72ff4cf9b92a1ff69b400770074b2a
d-laub <dlaub@ucsd.edu>
2023-08-20 15:32:04 -0700

    feat: initial Zarr reader.

--- genvarloader/bnfo/zarr.py
+++ genvarloader/bnfo/zarr.py
@@ -1,17 +1,40 @@
 from pathlib import Path
+from typing import Union
 
+import dask.array as da
 import xarray as xr
-import zarr
 
 from .types import Reader
 
 
 class Zarr(Reader):
-    def __init__(self, name: str, path: Path) -> None:
+    def __init__(self, name: str, path: Union[str, Path]) -> None:
+        """Read data from an XArray-compliant Zarr store. Should have contigs as
+        separate arrays and a list of contigs as an attribute.
+
+        Parameters
+        ----------
+        name : str
+            Name of the reader.
+        path : Path
+            Path to the store.
+        """
         self.name = name
         self.path = path
-        self.root = zarr.open_group(path)
-        # self._data = ...
+        self.ds = xr.open_zarr(path, mask_and_scale=False, concat_characters=False)
+        contigs = self.ds.attrs["contigs"]
+        shape = tuple(
+            s for d, s in self.ds.sizes.items() if not str(d).endswith("length")
+        )
+        dims = tuple(d for d in self.ds.sizes.keys() if not str(d).endswith("length"))
+        coords = {
+            d: c for d, c in self.ds.coords.items() if not str(d).endswith("length")
+        }
+        self.virtual_data = xr.DataArray(
+            da.empty(shape=shape, dtype=self.ds.dtypes[contigs[0]]),
+            dims=dims,
+            coords=coords,
+        )
 
     def read(self, contig: str, start: int, end: int) -> xr.DataArray:
-        return xr.DataArray(self.root[contig][start:end], dims=..., coords=...)
+        return self.ds[contig].isel(length=slice(start, end)).compute()
