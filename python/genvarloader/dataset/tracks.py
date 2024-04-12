fix: wrong layout out genotypes and wrong max ends computation.

python/genvarloader/dataset/tracks.py
@@ -1,72 +1,6 @@
-import json
-from pathlib import Path
-from typing import Dict, Union
-
 import numba as nb
 import numpy as np
-from attrs import define
 from numpy.typing import NDArray
-
-from ..utils import lengths_to_offsets
-
-
-@define
-class GenomeTrack:
-    """
-    Represents a genome-wide track at base-pair resolution.
-
-    Attributes
-    ----------
-    track : ndarray
-        Track data.
-    contigs : dict[str, int]
-        A dictionary mapping contig names to their length.
-    contig_offsets : ndarray
-        The offsets for each contig in the genome track data.
-    """
-
-    track: NDArray
-    contigs: Dict[str, int]
-    contig_offsets: NDArray[np.int32]
-    raw_contig_offsets: NDArray[np.int32]
-
-    def order_offsets_like(self, contigs: list[str]):
-        common, _, order = np.intersect1d(
-            contigs, list(self.contigs.keys()), return_indices=True
-        )
-        if len(common) != len(contigs):
-            raise ValueError("Not all contigs requested are in the track.")
-        self.contig_offsets = self.raw_contig_offsets[order]
-        return self
-
-    @classmethod
-    def from_path(cls, path: Union[str, Path]) -> "GenomeTrack":
-        """
-        Create a GenomeTrack from the specified path.
-
-        Parameters
-        ----------
-        path : Union[str, Path]
-            The path to the track data.
-
-        Returns
-        -------
-        track : GenomeTrack
-            A GenomeTrack object.
-
-        """
-        path = Path(path)
-        with open(path / "metadata.json") as f:
-            metadata = json.load(f)
-            contigs = metadata["contigs"]
-            non_length_shape = metadata["non_length_shape"]
-            dtype = np.dtype(metadata["dtype"])
-
-        contig_offsets = lengths_to_offsets(contigs.values())
-        track = np.memmap(path / "track.npy", mode="r", dtype=dtype).reshape(
-            *non_length_shape, -1
-        )
-        return cls(track, contigs, contig_offsets, contig_offsets)
 
 
 @nb.njit(parallel=True, nogil=True, cache=True)
@@ -280,7 +214,7 @@ def shift_and_realign_track(
 
 @nb.njit(parallel=True, nogil=True, cache=True)
 def shift_and_realign_tracks_sparse(
-    offset_idxs: NDArray[np.intp],
+    offset_idx: NDArray[np.intp],
     variant_idxs: NDArray[np.int32],
     offsets: NDArray[np.int32],
     regions: NDArray[np.int32],
@@ -295,7 +229,7 @@ def shift_and_realign_tracks_sparse(
 
     Parameters
     ----------
-    offset_idxs : NDArray[np.intp]
+    offset_idx : NDArray[np.intp]
         Shape = (regions, ploidy) Indices into offsets for each region.
     variant_idxs : NDArray[np.int32]
         Shape = (variants) Indices of variants.
@@ -324,7 +258,7 @@ def shift_and_realign_tracks_sparse(
         query_s = regions[query, 1]
 
         for hap in nb.prange(ploidy):
-            o_idx = offset_idxs[query, hap]
+            o_idx = offset_idx[query, hap]
             _out = out[query, hap]
             _shifts = shifts[query, hap]
 

python/genvarloader/dataset/write.py
@@ -7,7 +7,6 @@ from typing import TYPE_CHECKING, List, Optional, Tuple, Union, cast
 
 import numpy as np
 import polars as pl
-from einops import rearrange
 from loguru import logger
 from natsort import natsorted
 from tqdm.auto import tqdm
@@ -219,44 +218,44 @@ def write_variants(
 
             _contig = normalize_contig_name(contig, variants.records.contigs)
             if _contig is None:
-                genos = SparseGenotypes.empty(len(_samples), variants.ploidy, n_regions)
+                genos = SparseGenotypes.empty(n_regions, len(_samples), variants.ploidy)
             else:
                 multiplier = 2
                 while True:
                     records = variants.records.vars_in_range(_contig, starts, ends)
+                    # (s p v)
                     genos = variants.genotypes.read(
                         _contig,
                         records.start_idxs,
                         records.end_idxs,
                         sample_idx=sample_idx,
                     )
-                    # (v s p)
-                    genos = rearrange(genos, "s p v -> v s p")
-                    # (v s p)
-                    ilens = np.where(genos == 1, records.size_diffs[:, None, None], 0)
-                    # (r s p)
-                    ilens = np.add.reduceat(ilens, records.offsets[:-1], axis=0)
-                    # (r s p)
-                    effective_length = (ends - starts)[:, None, None] + ilens
-                    # (r s p)
+                    # (s p v)
+                    ilens = np.where(genos == 1, records.size_diffs, 0)
+                    # (s p r)
+                    ilens = np.add.reduceat(ilens, records.offsets[:-1], axis=-1)
+                    # (s p r)
+                    effective_length = (ends - starts) + ilens
+                    # (s p r)
                     missing_length = region_length - effective_length
                     if np.all(missing_length <= 0):
                         break
                     # (r)
-                    ends += multiplier * missing_length.max((1, 2))
+                    ends += multiplier * missing_length.max((0, 1))
 
                 pbar.set_description(
                     f"Sparsifying genotypes for {part.height} regions on {contig}"
                 )
                 genos, c_max_ends = SparseGenotypes.from_dense_with_length(
-                    genos,
+                    genos=genos,
                     # make indices relative to the contig
-                    records.start_idxs - variants.records.contig_offsets[_contig],
-                    records.offsets.astype(np.int32),
-                    variants.records.v_diffs[_contig],
-                    variants.records.v_starts[_contig],
-                    starts,
-                    region_length,
+                    first_v_idxs=records.start_idxs
+                    - variants.records.contig_offsets[_contig],
+                    offsets=records.offsets.astype(np.int32),
+                    ilens=variants.records.v_diffs[_contig],
+                    positions=variants.records.v_starts[_contig],
+                    starts=starts,
+                    length=region_length,
                 )
                 max_ends[last_max_end_idx : last_max_end_idx + n_regions] = c_max_ends
                 last_max_end_idx += n_regions

python/genvarloader/variants/genotypes.py
@@ -714,8 +714,8 @@ class VCFGenos(Genotypes):
         else:
             _haplotype_idx = np.atleast_1d(np.asarray(haplotype_idx, dtype=int))
 
-        # (s p v)
         n_variants = (end_idxs - start_idxs).sum()
+        # (s p v)
         genos = np.empty(
             (len(self.samples), self.ploidy, n_variants),
             dtype=np.int8,
@@ -736,9 +736,9 @@ class VCFGenos(Genotypes):
             if overlapping_query_intervals.any():
                 # (n_valid)
                 place_idx = geno_idxs[overlapping_query_intervals]
+                genos[..., place_idx] = v.genotype.array()[:, :2, None]
                 # increment idxs for next iteration
                 geno_idxs[overlapping_query_intervals] += 1
-                genos[..., place_idx] = v.genotype.array()[:, :2, None]
             if (geno_idxs == finish_idxs).all():
                 break
 

tests/dataset/genotypes/test_dense2sparse.py
@@ -1,12 +1,13 @@
+from typing import Tuple
+
 import numpy as np
 from einops import repeat
-from pytest_cases import fixture
-
 from genvarloader.dataset.genotypes import DenseGenotypes, SparseGenotypes
+from pytest_cases import fixture
 
 
 @fixture
-def dense_genos() -> DenseGenotypes:
+def genos():
     n_samples = 2
     genos = np.array(
         [
@@ -16,36 +17,52 @@ def dense_genos() -> DenseGenotypes:
         ],
         np.int8,
     )
-    genos = repeat(genos, "v p -> (s v) p", s=n_samples)
+    genos = repeat(genos, "v p -> s p v", s=n_samples)
     first_v_idxs = np.array([0], np.uint32)
     offsets = np.array([0, 3], np.uint32)
-    return DenseGenotypes(
+
+    sparse_v_idxs = np.array([2, 1, 2, 1], np.int32)
+    sparse_offsets = np.array([0, 1, 2, 3, 4], np.int32)
+    dense = DenseGenotypes(
         genos=genos, first_v_idxs=first_v_idxs, offsets=offsets, n_samples=n_samples
     )
-
-
-def test_dense2sparse(dense_genos: DenseGenotypes):
-    n_samples = dense_genos.n_samples
-    desired_genos = np.array(
-        [
-            [0, 1],
-            [1, -9],
-        ],
-        np.int8,
-    )
-    desired_genos = repeat(desired_genos, "v p -> (s v) p", s=n_samples)
-    desired = SparseGenotypes(
-        genos=desired_genos,
-        variant_idxs=np.array([1, 2, 1, 2], np.uintp),
-        offsets=np.array([0, 2, 4], np.uintp),
-        n_samples=n_samples,
+    sparse = SparseGenotypes(
+        variant_idxs=sparse_v_idxs,
+        offsets=sparse_offsets,
         n_regions=1,
+        n_samples=n_samples,
+        ploidy=2,
     )
+    return dense, sparse
 
-    actual = dense_genos.to_sparse()
 
-    np.testing.assert_equal(actual.genos, desired.genos)
+def test_from_dense(genos: Tuple[DenseGenotypes, SparseGenotypes]):
+    dense, desired = genos
+
+    actual = SparseGenotypes.from_dense(dense.genos, dense.first_v_idxs, dense.offsets)
+
     np.testing.assert_equal(actual.variant_idxs, desired.variant_idxs)
     np.testing.assert_equal(actual.offsets, desired.offsets)
-    assert actual.n_samples == desired.n_samples
-    assert actual.n_regions == desired.n_regions
+
+
+def test_from_dense_with_length(genos: Tuple[DenseGenotypes, SparseGenotypes]):
+    dense, desired = genos
+    ilens = np.array([0, 0, -3], np.int32)
+    positions = np.array([0, 1, 2], np.int32)
+    starts = np.array([0], np.int32)
+    length = 3
+    desired_max_ends = np.array([6], np.int32)
+
+    actual, max_ends = SparseGenotypes.from_dense_with_length(
+        dense.genos,
+        dense.first_v_idxs,
+        dense.offsets,
+        ilens,
+        positions,
+        starts,
+        length,
+    )
+
+    np.testing.assert_equal(actual.variant_idxs, desired.variant_idxs)
+    np.testing.assert_equal(actual.offsets, desired.offsets)
+    np.testing.assert_equal(max_ends, desired_max_ends)