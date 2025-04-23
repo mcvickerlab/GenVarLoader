d773a2529041324c519b7fcf79971b99419e72b8
d-laub <dlaub@ucsd.edu>
2025-04-22 17:48:56 -0700

    feat: SVAR support passes all tests

--- python/genvarloader/_ragged.py
+++ python/genvarloader/_ragged.py
@@ -1,19 +1,16 @@
 from __future__ import annotations
 
-from typing import Any, Generic, Optional, Tuple, TypeGuard, TypeVar, Union, cast
+from typing import Any, Optional, Tuple, TypeGuard, TypeVar, Union
 
-import awkward as ak
 import numba as nb
 import numpy as np
 from attrs import define
-from awkward.contents import ListOffsetArray, NumpyArray, RegularArray
-from awkward.index import Index64
 from einops import repeat
 from numpy.typing import NDArray
-from typing_extensions import Self
+from seqpro._ragged import Ragged
 
-from ._types import DTYPE, AnnotatedHaps, Idx
-from ._utils import _lengths_to_offsets, idx_like_to_array
+from ._types import DTYPE, AnnotatedHaps
+from ._utils import _lengths_to_offsets
 
 __all__ = ["Ragged", "RaggedIntervals", "INTERVAL_DTYPE", "pad_ragged"]
 
@@ -31,9 +28,9 @@ class RaggedAnnotatedHaps:
         return self.haps.shape
 
     def to_padded(self) -> AnnotatedHaps:
-        haps = self.haps.to_padded(b"N")
-        var_idxs = self.var_idxs.to_padded(-1)
-        ref_coords = self.ref_coords.to_padded(-1)
+        haps = to_padded(self.haps, b"N")
+        var_idxs = to_padded(self.var_idxs, -1)
+        ref_coords = to_padded(self.ref_coords, -1)
         return AnnotatedHaps(haps, var_idxs, ref_coords)
 
     def reshape(self, shape: tuple[int, ...]) -> RaggedAnnotatedHaps:
@@ -61,254 +58,53 @@ def is_rag_dtype(rag: Ragged, dtype: type[DTYPE]) -> TypeGuard[Ragged[DTYPE]]:
     return np.issubdtype(rag.data.dtype, dtype)
 
 
-@define
-class Ragged(Generic[RDTYPE]):
-    """Ragged array i.e. a rectilinear array where the final axis is ragged. Should not be initialized
-    directly, use :meth:`from_offsets()` or :meth:`from_lengths()` instead.
-    Does not have a :code:`__getitem__` method currently.
-
-    Examples
-    --------
-
-    .. code-block:: python
-
-        r = Ragged.from_lengths(np.arange(10), np.array([3, 2, 5]))
-        assert r.offsets ==  np.array([0, 3, 5, 10])
-        assert r.data[r.offsets[0]:r.offsets[1]] == np.array([0, 1, 2])
-        assert r.data[r.offsets[1]:r.offsets[2]] == np.array([3, 4])
-        assert r.data[r.offsets[2]:r.offsets[3]] == np.array([5, 6, 7, 8, 9])
-
-    """
-
-    data: NDArray[RDTYPE]
-    """A 1D array of the data."""
-    shape: Tuple[int, ...]
-    """Shape of the ragged array, excluding the length dimension. For example, if
-        the shape is (2, 3), then the j, k-th element can be mapped to an index for
-        offsets with :code:`i = np.ravel_multi_index((j, k), shape)`. The number of ragged
-        elements corresponds to the product of the shape."""
-    maybe_offsets: Optional[NDArray[np.int64]] = None
-    maybe_lengths: Optional[NDArray[np.int32]] = None
-
-    def __attrs_post_init__(self):
-        if self.maybe_offsets is None and self.maybe_lengths is None:
-            raise ValueError("Either offsets or lengths must be provided.")
-
-    def __len__(self):
-        return self.shape[0]
-
-    def item(self):
-        a = self.squeeze()
-        if a.shape != ():
-            raise ValueError("Array has more than 1 ragged element.")
-        return a.data
-
-    @property
-    def ndim(self) -> int:
-        """Number of dimensions of the ragged array."""
-        return len(self.shape)
-
-    @property
-    def offsets(self) -> NDArray[np.int64]:
-        """Offsets into the data array to get corresponding elements. The i-th element
-        is accessible as :code:`data[offsets[i]:offsets[i+1]]`."""
-        if self.maybe_offsets is None:
-            self.maybe_offsets = _lengths_to_offsets(self.lengths)
-        return self.maybe_offsets
-
-    @property
-    def lengths(self) -> NDArray[np.int32]:
-        """Array with appropriate shape containing lengths of each element in the ragged array."""
-        if self.maybe_lengths is None:
-            self.maybe_lengths = np.diff(self.offsets).reshape(self.shape)
-        return self.maybe_lengths
-
-    @classmethod
-    def from_offsets(
-        cls,
-        data: NDArray[DTYPE],
-        shape: Union[int, Tuple[int, ...]],
-        offsets: NDArray[np.int64],
-    ) -> Self:
-        """Create a Ragged array from data and offsets.
-
-        Parameters
-        ----------
-        data
-            1D data array.
-        shape
-            Shape of the ragged array, excluding the length dimension.
-        offsets
-            Offsets into the data array to get corresponding elements.
-        """
-        if isinstance(shape, int):
-            shape = (shape,)
-        return cls(data, shape, maybe_offsets=offsets)
-
-    @classmethod
-    def from_lengths(cls, data: NDArray[DTYPE], lengths: NDArray[np.int32]) -> Self:
-        """Create a Ragged array from data and lengths. The lengths array should have
-        the intended shape of the Ragged array.
-
-        Parameters
-        ----------
-        data
-            1D data array.
-        lengths
-            Lengths of each element in the ragged array.
-        """
-        return cls(data, lengths.shape, maybe_lengths=lengths)
-
-    @staticmethod
-    def concat(*arrays: "Ragged[DTYPE]", axis: int) -> "Ragged[DTYPE]":
-        # """Concatenate multiple Ragged arrays along a given axis."""
-        # need to check whether this would lead to incorrect indexing
-        raise NotImplementedError
-        if len(set((*a.shape[:axis], *a.shape[axis + 1 :]) for a in arrays)) != 1:
-            raise ValueError(
-                f"All arrays must have the same shape except along axis {axis}."
-            )
-
-        if len(set(a.data.dtype for a in arrays)) != 1:
-            raise ValueError("All arrays must have the same dtype.")
-
-        data = np.concatenate([a.data for a in arrays])
-        lengths = np.concatenate([a.lengths for a in arrays], axis=axis)
-        return Ragged.from_lengths(data, lengths)
-
-    @staticmethod
-    def stack(*arrays: "Ragged[DTYPE]") -> "Ragged[DTYPE]":
-        """Stack multiple ragged arrays along a new first axis."""
-        if len(set(a.shape for a in arrays)) != 1:
-            raise ValueError("All arrays must have the same shape.")
-
-        if len(set(a.data.dtype for a in arrays)) != 1:
-            raise ValueError("All arrays must have the same dtype.")
-
-        data = np.concatenate([a.data for a in arrays])
-        lengths = np.stack([a.lengths for a in arrays], axis=0)
-        return Ragged.from_lengths(data, lengths)
-
-    def to_padded(self, pad_value: Any) -> NDArray[RDTYPE]:
-        """Convert this Ragged array to a rectilinear array by right-padding each entry with a value.
-        The final axis will have the maximum length across all entries.
-
-        Parameters
-        ----------
-        pad_value
-            Value to pad the entries with.
-
-        Returns
-        -------
-            Padded array with shape :code:`(*self.shape, self.lengths.max())`.
-        """
-        length = self.lengths.max()
-        shape = (*self.shape, length)
-        if self.data.dtype.str == "|S1":
-            if isinstance(pad_value, (str, bytes)):
-                if len(pad_value) != 1:
-                    raise ValueError(
-                        "Tried padding an S1 array with a `pad_value` that is multiple characters."
-                    )
-                pad_value = np.uint8(ord(pad_value))
-            elif isinstance(pad_value, int):
-                if pad_value < 0 or pad_value > 255:
-                    raise ValueError(
-                        "Tried padding an S1 array with an integer `pad_value` outside the ASCII range."
-                    )
-                pad_value = np.uint8(pad_value)
-            else:
-                raise ValueError(
-                    "Tried padding an S1 array with a `pad_value` that isn't a string, byte, or integer."
-                )
-            padded = np.empty((np.prod(shape[:-1]), shape[-1]), dtype=np.uint8)
-            pad_ragged(self.data.view(np.uint8), self.offsets, pad_value, padded)
-            padded = padded.view(self.data.dtype).reshape(shape)
-        else:
-            padded = np.empty((np.prod(shape[:-1]), shape[-1]), dtype=self.data.dtype)
-            pad_ragged(self.data, self.offsets, pad_value, padded)
-            padded = padded.reshape(shape)
-
-        return padded
-
-    def squeeze(self, axis: Optional[Union[int, Tuple[int, ...]]] = None) -> Self:
-        """Squeeze the ragged array along the given non-ragged axis."""
-        return Ragged.from_lengths(self.data, self.lengths.squeeze(axis))
-
-    def reshape(self, shape: Tuple[int, ...]) -> Self:
-        """Reshape non-ragged axes."""
-        # this is correct because all reshaping operations preserve the layout i.e. raveled ordered
-        return Ragged.from_lengths(self.data, self.lengths.reshape(shape))
-
-    def __str__(self):
-        return (
-            f"Ragged<shape={self.shape} dtype={self.data.dtype} size={self.data.size}>"
-        )
-
-    def __getitem__(self, idx: Union[Idx, Tuple[Idx, ...]]):
-        if not isinstance(idx, tuple):
-            idx = (idx,)
-
-        if len(idx) < len(self.shape):
-            raise IndexError(
-                f"Too few indices for array: expected {len(self.shape)}, got {len(idx)}."
-            )
-
-        idx = tuple(
-            idx_like_to_array(_idx, self.shape[i]) for i, _idx in enumerate(idx)
-        )
-        idx = np.ravel_multi_index(idx, self.shape).squeeze()
-
-        return self.data[self.offsets[idx] : self.offsets[idx + 1]]
-
-    def to_awkward(self) -> ak.Array:
-        """Convert to an `Awkward <https://awkward-array.org/doc/main/>`_ array without copying. Note that this effectively
-        returns a view of the data, so modifying the data will modify the original array."""
-        layout = ListOffsetArray(
-            Index64(self.offsets),
-            NumpyArray(self.data),  # type: ignore | NDArray[RDTYPE] is ArrayLike
-        )
-
-        for size in reversed(self.shape[1:]):
-            layout = RegularArray(layout, size)
-
-        return ak.Array(layout)
-
-    @classmethod
-    def from_awkward(cls, awk: "ak.Array") -> Self:
-        """Convert from an `Awkward <https://awkward-array.org/doc/main/>`_ array without copying. Note that this effectively
-        returns a view of the data, so modifying the data will modify the original array."""
-        # parse shape
-        shape_str = awk.typestr.split(" * ")
-        try:
-            shape = tuple(map(int, shape_str[:-2]))
-        except ValueError as err:
-            raise ValueError(
-                f"Only the final axis of an awkward array may be variable to convert to ragged, but got {awk.type}."
-            ) from err
-
-        # extract data and offsets
-        data = ak.flatten(awk, axis=None).to_numpy()
-        layout = awk.layout
-        while hasattr(layout, "content"):
-            if isinstance(layout, ListOffsetArray):
-                offsets = layout.offsets.data
-                offsets = cast(NDArray[np.int64], offsets)
-                rag = cls.from_offsets(data, shape, offsets)
-                break
-            else:
-                layout = layout.content
-        else:
-            lengths = ak.count(awk, axis=-1).to_numpy()
-            rag = cls.from_lengths(data, lengths)
-
-        return rag
-
-
 INTERVAL_DTYPE = np.dtype(
     [("start", np.int32), ("end", np.int32), ("value", np.float32)], align=True
 )
 RaggedIntervals = Ragged[np.void]
+
+
+def to_padded(rag: Ragged[RDTYPE], pad_value: Any) -> NDArray[RDTYPE]:
+    """Convert this Ragged array to a rectilinear array by right-padding each entry with a value.
+    The final axis will have the maximum length across all entries.
+
+    Parameters
+    ----------
+    pad_value
+        Value to pad the entries with.
+
+    Returns
+    -------
+        Padded array with shape :code:`(*self.shape, self.lengths.max())`.
+    """
+    length = rag.lengths.max()
+    shape = (*rag.shape, length)
+    if rag.data.dtype.str == "|S1":
+        if isinstance(pad_value, (str, bytes)):
+            if len(pad_value) != 1:
+                raise ValueError(
+                    "Tried padding an S1 array with a `pad_value` that is multiple characters."
+                )
+            pad_value = np.uint8(ord(pad_value))
+        elif isinstance(pad_value, int):
+            if pad_value < 0 or pad_value > 255:
+                raise ValueError(
+                    "Tried padding an S1 array with an integer `pad_value` outside the ASCII range."
+                )
+            pad_value = np.uint8(pad_value)
+        else:
+            raise ValueError(
+                "Tried padding an S1 array with a `pad_value` that isn't a string, byte, or integer."
+            )
+        padded = np.empty((np.prod(shape[:-1]), shape[-1]), dtype=np.uint8)
+        pad_ragged(rag.data.view(np.uint8), rag.offsets, pad_value, padded)
+        padded = padded.view(rag.data.dtype).reshape(shape)
+    else:
+        padded = np.empty((np.prod(shape[:-1]), shape[-1]), dtype=rag.data.dtype)
+        pad_ragged(rag.data, rag.offsets, pad_value, padded)
+        padded = padded.reshape(shape)
+
+    return padded
 
 
 @nb.njit(parallel=True, nogil=True, cache=True)
@@ -318,8 +114,13 @@ def pad_ragged(
     pad_value: DTYPE,
     out: NDArray[DTYPE],
 ):
-    for i in nb.prange(len(offsets) - 1):
-        start, end = offsets[i], offsets[i + 1]
+    for i in nb.prange(len(offsets)):
+        if offsets.ndim == 1:
+            if i == len(offsets) - 1:
+                continue
+            start, end = offsets[i], offsets[i + 1]
+        else:
+            start, end = offsets[i]
         entry_len = end - start
         out[i, :entry_len] = data[start:end]
         out[i, entry_len:] = pad_value
