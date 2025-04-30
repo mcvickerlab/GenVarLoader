d873f4a8cf53ae9f3272a9e4e4d437ab387b8376
d-laub <dlaub@ucsd.edu>
2025-04-29 21:31:19 -0700

    fix: type annotation

--- python/genvarloader/_dataset/_reconstruct.py
+++ python/genvarloader/_dataset/_reconstruct.py
@@ -570,7 +570,7 @@ class Haps(Reconstructor[H]):
 
 @define
 class Tracks(Reconstructor[Ragged[np.float32]]):
-    intervals: Dict[str, RaggedIntervals]
+    intervals: Dict[str, Ragged[np.void]]
     """The intervals in the dataset. This is memory mapped."""
     active_tracks: List[str]
 
@@ -590,7 +590,7 @@ class Tracks(Reconstructor[Ragged[np.float32]]):
             else:
                 available_tracks.append(p.name)
         available_tracks.sort()
-        intervals: Optional[Dict[str, RaggedIntervals]] = {}
+        intervals: Optional[Dict[str, Ragged[np.void]]] = {}
         for track in available_tracks:
             itvs = np.memmap(
                 path / "intervals" / track / "intervals.npy",
